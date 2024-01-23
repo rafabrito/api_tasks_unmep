891848b2730033e0445fe0315441bd1dfa94125e
rafabrito <rafa.brito295@gmail.com>
2024-01-23 19:15:51 -0300

    mudança nas configurações do banco de dados e no README.md

--- api/config.php
+++ api/config.php
@@ -4,15 +4,15 @@ define('APP NAME',          'UNMEP backend');
 define('APP_VERSION',       '1.0.0');
 
 // // MYSQL
-// define('DB_HOST',      $_ENV['DB_HOST']);
-// define('DB_DBNAME',    $_ENV['DB_DBNAME']);
-// define('DB_USER',      $_ENV['DB_USER']);
-// define('DB_PASSWORD',  $_ENV['DB_PASSWORD']);
-// define('DB_CHARSET',   $_ENV['DB_CHARSET']);
+define('DB_HOST',      $_ENV['DB_HOST']);
+define('DB_DBNAME',    $_ENV['DB_DBNAME']);
+define('DB_USER',      $_ENV['DB_USER']);
+define('DB_PASSWORD',  $_ENV['DB_PASSWORD']);
+define('DB_CHARSET',   $_ENV['DB_CHARSET']);
 
 // MYSQL
-define('DB_HOST',      'localhost');
-define('DB_DBNAME',    'nome_banco_dados');
-define('DB_USER',      'root');
-define('DB_PASSWORD',  '');
-define('DB_CHARSET',   'utf8');
+// define('DB_HOST',      'localhost');
+// define('DB_DBNAME',    'nome_banco_dados');
+// define('DB_USER',      'root');
+// define('DB_PASSWORD',  '');
+// define('DB_CHARSET',   'utf8');
